d580242c073e0b486bc73d298627e6f56e7cc8a2
taufik69 <taufik35-2915@diu.edu.bd>
2025-06-27 14:36:06 +0600

    @Fixed mongodb url fixed

--- src/database/db.js
+++ src/database/db.js
@@ -3,9 +3,7 @@ const { customError } = require("../lib/CustomError");
 const { dbConfig } = require("../constant/constant");
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(
-      `${process.env.DATABASE_URL}/${dbConfig.databaseName}`
-    );
+    const conn = await mongoose.connect(`${process.env.DATABASE_URL}`);
     console.log(conn.connection.host, " database connected");
   } catch (error) {
     throw new customError("Database connection error", 500);
