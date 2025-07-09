2b326e6280cf0178fb5a2336145f34c0198fecfb
taufik69 <taufik35-2915@diu.edu.bd>
2025-07-09 15:34:28 +0600

    cors whitelisting

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
  */
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3001",
+    origin: [process.env.FRONTEND_URL, "http://localhost:3001"],
     credentials: true,
   })
 );
