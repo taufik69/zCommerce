aafff8d28a4e1475cff0fb88556ad6f9d495d039
wasimmahamod <wasimmahamodraza@gmail.com>
2025-08-03 14:15:53 +0600

    cors origin add 3000 and also modify forntend env url

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(
       process.env.FRONTEND_URL,
       "http://localhost:3001",
       "http://localhost:5173",
+      "http://localhost:3000",
       "http://localhost:5172",
       "http://localhost:5174",
     ],
