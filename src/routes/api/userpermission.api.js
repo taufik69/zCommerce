06013b0332913c149799c55b92e78e9ec4296655
Shohelrana63 <shohelrana.beg@gmail.com>
2025-11-05 01:41:07 +0600

    fix: auth guard comment

--- src/routes/api/userpermission.api.js
+++ src/routes/api/userpermission.api.js
@@ -4,18 +4,18 @@ const userpermissionController = require("../../controller/adduserpermission.con
 const { authGuard } = require("../../middleware/authMiddleware");
 const { authorize } = require("../../middleware/checkPermission.middleware");
 _.route("/get-all-permissions").get(
-  authGuard,
-  authorize("userroleandpermission", "view"),
+  // authGuard,
+  // authorize("userroleandpermission", "view"),
   userpermissionController.getAllPermissions
 );
 _.route("/get-all-users").get(
-  authGuard,
-  authorize("userroleandpermission", "view"),
+  // authGuard,
+  // authorize("userroleandpermission", "view"),
   userpermissionController.getAllUsers
 );
 _.route("/get-user-permissions/:userId").get(
-  authGuard,
-  authorize("userroleandpermission", "view"),
+  // authGuard,
+  // authorize("userroleandpermission", "view"),
   userpermissionController.getUserPermissions
 );
 _.route("/update-user-permissions/:userId").put(
